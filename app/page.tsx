13681aeaf2bad258ea9d6da51f945a4bb3b6b551
myeong-ga <jaehyulee39@gmail.com>
2025-03-21 00:18:46 +0900

    chore: update README.md

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  //redirect('/use-chat-human-in-the-loop');
-  //redirect('/use-chat-streamdata-multistep');
 
+  //redirect('/use-chat-human-in-the-loop');
+
+  //redirect('/use-chat-streamdata-multistep');
+ 
   redirect('/use-chat-persistence-single-message-image-output');
   
 }
